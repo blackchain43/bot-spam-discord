6484fcdcf1c376bde112a0dc0dd8455e7ca84bc3
hqphuoc <hqphuoc@tmproxy.com>
2024-01-18 14:10:59 +0700

    add multi thread

--- auto.py
+++ auto.py
@@ -1,10 +1,13 @@
-from keep_alive import keep_alive
+from keep_alive import run
 import http.client
 import json
 import random
 import sys
 from time import sleep
 from http.client import HTTPSConnection
+from threading import Thread
+import certifi
+import ssl
 
 # Open file containing user information
 with open("info.txt", "r") as file:
@@ -16,9 +19,13 @@ def configure_info():
         user_id = input("User-ID: ")
         token = input("Discord token: ")
         channel_url = input("Discord channel URL: ")
-        channel_id = input("Discord channel ID: ")
+        channel_cn_id = input("Discord channel CN ID: ")
+        channel_jp_id = input("Discord channel JP ID: ")
+        channel_vn_id = input("Discord channel VN ID: ")
         with open("info.txt", "w") as file:
-            file.write(f"{user_id}\n{token}\n{channel_url}\n{channel_id}")
+            file.write(
+                f"{user_id}\n{token}\n{channel_url}\n{channel_cn_id}\n{channel_jp_id}\n{channel_vn_id}"
+            )
     except Exception as e:
         print(f"Error configuring user information: {e}")
         exit()
@@ -28,9 +35,13 @@ def set_channel():
     user_id = text[0]
     token = text[1]
     channel_url = input("Discord channel URL: ")
-    channel_id = input("Discord channel ID: ")
+    channel_cn_id = input("Discord channel ID: ")
+    channel_jp_id = input("Discord channel ID: ")
+    channel_vn_id = input("Discord channel ID: ")
     with open("info.txt", "w") as file:
-        file.write(f"{user_id}\n{token}\n{channel_url}\n{channel_id}")
+        file.write(
+            f"{user_id}\n{token}\n{channel_url}\n{channel_cn_id}\n{channel_jp_id}\n{channel_vn_id}"
+        )
 
 
 def show_help():
@@ -57,7 +68,7 @@ if len(sys.argv) > 1:
         show_help()
         exit()
 
-if len(text) != 4:
+if len(text) != 6:
     print(
         "An error inside the user information file. Please ensure the file contains the following information in order: User agent, Discord token, Discord channel URL, and Discord channel ID and try again ->python3 auto.py"
     )
@@ -76,7 +87,11 @@ print("Messages will be sent to " + header_data["referrer"] + ".")
 
 
 def get_connection():
-    return HTTPSConnection("discordapp.com", 443)
+    return HTTPSConnection(
+        "discordapp.com",
+        443,
+        context=ssl.create_default_context(cafile=certifi.where()),
+    )
 
 
 def send_message(conn, channel_id, message_data):
@@ -87,7 +102,7 @@ def send_message(conn, channel_id, message_data):
         resp = conn.getresponse()
 
         if 199 < resp.status < 300:
-            print("Message sent!")
+            print("Message sent on channel " + channel_id)
     except Exception as e:
         print(f"Error sending message: {e}")
 
@@ -110,24 +125,47 @@ def get_latest_message(conn, channel_id):
         return ""
 
 
+def send_messages_in_thread(channel_id, message_file_name):
+    print(f"Reading file {message_file_name}")
+    with open(message_file_name, "r", encoding="utf8") as file:
+        messages = file.read().splitlines()
+        # Loop through messages and send them
+        for message in messages:
+            random_wait_time_coefficient = random.random()
+            message_data = json.dumps({"content": message})
+            conn = get_connection()
+            latest_id = get_latest_message(conn, channel_id)
+            if latest_id != "":
+                message_data = json.dumps(
+                    {"content": message, "message_reference": {"message_id": latest_id}}
+                )
+            send_message(conn, channel_id, message_data)
+            conn.close()
+
+            print(f"Waiting {wait_time*(1+random_wait_time_coefficient)} seconds...")
+            sleep(wait_time * (1 + random_wait_time_coefficient))
+
+
 # Read messages from file
-with open("messages.txt", "r", encoding="utf8") as file:
-    messages = file.read().splitlines()
+def keep_alive(n, file_names):
+    threads = []
+    for i in range(n):
+        t = Thread(
+            target=send_messages_in_thread,
+            args=(
+                text[3 + i],
+                file_names[i],
+            ),
+        )
+        threads.append(t)
+        t.start()
+        sleep(wait_time)
+
+    for t in threads:
+        t.join()
+
 
 # Read wait times from user
 wait_time = int(input("Seconds between messages: "))
-keep_alive()
-# Loop through messages and send them
-for message in messages:
-    message_data = json.dumps({"content": message})
-    conn = get_connection()
-    latest_id = get_latest_message(conn, text[3])
-    if latest_id != "":
-        message_data = json.dumps(
-            {"content": message, "message_reference": {"message_id": latest_id}}
-        )
-    send_message(conn, text[3], message_data)
-    conn.close()
-
-    print(f"Waiting {wait_time} seconds...")
-    sleep(wait_time)
+keep_alive(3, ["messages_cn.txt", "messages_jp.txt", "messages_vn.txt"])
+# keep_alive(2, ["messages_jp.txt","messages_vn.txt"])
